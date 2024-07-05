b8de9171510297bc635db564ee1080b8f55349cc
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-07-05 04:26:20 -0400

    Set Up Routing and Basic styles

--- e-commerce-storefront/src/App.js
+++ e-commerce-storefront/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import ProductPage from './pages/ProductPage/ProductPage';
+import CartPage from './pages/CartPage/CartPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/cart">Cart</Link>
+      </nav>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductPage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ e-commerce-storefront/src/pages/ProductPage/ProductPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductPage.css';
+
+const ProductPage = () => {
+    return (
+        <div className="product-page">
+            <h1>Product Page</h1>
+        </div>
+    );
+};
+
+export default ProductPage;
